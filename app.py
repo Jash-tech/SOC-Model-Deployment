0429c0050cf7e0b3e6260aeea6c7e9e6781b7957
Jash Suke <54662527+Jash-tech@users.noreply.github.com>
2022-02-22 19:47:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ def predict():
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
-    output = round(prediction[0], 2)
+    
 
-    return render_template('index.html', prediction_text='SOC of Battery'.format(output))
+    return render_template('index.html', prediction_text='SOC of Battery:'.format(prediction))
 
 
 if __name__ == "__main__":
